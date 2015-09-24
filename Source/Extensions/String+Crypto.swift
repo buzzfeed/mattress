cdd2a384ee7ecb8ea527fb698179cb274e8162d0
Kevin Lord <kevin.lord@buzzfeed.com>
2015-09-24 11:51:04 -0400

    Fix MD5 hashing for disk cache filenames on iOS 8

--- Source/Extensions/String+Crypto.swift
+++ Source/Extensions/String+Crypto.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 extension String {
-    func MD5() -> String? {
-        return (self as NSString).dataUsingEncoding(NSUTF8StringEncoding)?.MD5().hexString()
+    func mattress_MD5() -> String? {
+        return (self as NSString).dataUsingEncoding(NSUTF8StringEncoding)?.mattress_MD5().mattress_hexString()
     }
 
-    func SHA1() -> String? {
-        return (self as NSString).dataUsingEncoding(NSUTF8StringEncoding)?.SHA1().hexString()
+    func mattress_SHA1() -> String? {
+        return (self as NSString).dataUsingEncoding(NSUTF8StringEncoding)?.mattress_SHA1().mattress_hexString()
     }
 }
